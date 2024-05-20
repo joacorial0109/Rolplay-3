95aef06d4ad8301b429e2a048c6da5423dc7cfec
mapachexx <fernandezfelipemateo@gmail.com>
2024-05-20 17:13:03 -0300

    enemigos

--- src/Library/Enemigos/Racoon.cs
+++ src/Library/Enemigos/Racoon.cs
@@ -17,6 +17,24 @@ namespace RoleplayGame.Characters
         /// Obtiene un valor que indica el ataque total del personaje a través de su arma y/o bonificación.
         /// </summary>
         /// <value>Ataque del personaje.</value>
+        
+         public IDefenseItem DefenseItem { get; set; }
+
+        /// <summary>
+        /// Obtiene un valor que indica el ataque total del personaje a través de su arma.
+        /// </summary>
+        /// <value>Ataque del arma.</value>
+
+           public override int DefenseValue
+        {
+            get
+            {
+                if (DefenseItem == null) return 0;
+                return DefenseItem.DefenseValue;
+            }
+        }
+        
+
         public override int AttackValue
         {
             get
@@ -30,18 +48,20 @@ namespace RoleplayGame.Characters
         /// Obtiene un valor que indica la defensa total del personaje a través de su ítem defensivo y/o bonificación.
         /// </summary>
         /// <value>Defensa del personaje.</value>
-        public override int DefenseValue
+     
+     
+      /*  public override int DefenseValue
         {
             get
             {
                 return 10;
             }
-        }
+        }*/
 
         /// <summary>
-        /// Método constructor de DarkKnight´s.
+        /// Método constructor de RACOON.
         /// </summary>
-        /// <param name="name">Nombre de DarkKnight.</param>
+        /// <param name="name">Nombre de RACOON.</param>
         /// 
         public Racoon(string name) : base(name)
         {

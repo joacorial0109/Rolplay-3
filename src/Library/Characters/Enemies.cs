ee68d955114b02f5f4e53c0587a783dea3af2c7a
enzobueno11 <127800256+enzobueno11@users.noreply.github.com>
2024-05-20 15:54:06 -0300

    cambios

--- src/Library/Characters/Enemies.cs
+++ src/Library/Characters/Enemies.cs
@@ -1,34 +1,38 @@
 namespace RoleplayGame.Characters
 {
     /// <summary>
-    /// Clase base de los héroes; hereda de la clase Character. Es una clase abstracta, por lo tanto, no se pueden crear instancias de ella.
+    /// Clase base de los enemigos; hereda de la clase Character. Es una clase abstracta, por lo tanto, no se pueden crear instancias de ella.
+    /// Los enemigos tienen bonificaciones en ataque y defensa.
     /// </summary>
-    public abstract class  Enemies : Character
+    public abstract class Enemy : Character
     {
         /// <summary>
-        /// Puntos de victoria acumulados.
+        /// Puntos de victoria del enemigo.
         /// </summary>
-        protected int _acumulatedVP = 0;
-
-        /// <summary>
-        /// Obtiene un valor que representa los puntos de victoria del héroe.
-        /// </summary>
-        /// <value></value>
-        public int AcumulatedVP { get { return _acumulatedVP; } }
+        protected int _VP;
 
         /// <summary>
         /// Método constructor que hereda la implementación de su clase base Character.
         /// </summary>
-        /// <param name="name">Nombre del héroe.</param>
-        public Hero(string name) : base(name) { }
+        /// <param name="name">Nombre del enemigo.</param>
+        public Enemy(string name) : base(name) { }
 
         /// <summary>
-        /// Obtiene los puntos de victoria del enemigo.
+        /// Pierde y retorna sus puntos de victoria.
         /// </summary>
-        /// <param name="VPofEnemy"></param>
-        public void ObtainVP(int VPofEnemy)
+        /// <returns>Si la vida es cero, pierde y retorna sus puntos de victoria, sino retorna cero.</returns>
+        public int GiveVP()
         {
-            _acumulatedVP += VPofEnemy;
+            if (this.Health == 0)
+            {
+                int VPtoReturn = _VP;
+                _VP = 0;
+                return VPtoReturn;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
